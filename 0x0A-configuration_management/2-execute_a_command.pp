0371de04fe5574104ac6bbcd0c1a5974392adde8
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-22 18:07:13 -0700

    execute a command

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,10 @@
+# File: 2-execute_a_command.pp
+
+# Define an exec resource to kill the process named "killmenow"
+
+exec{ 'killmenow':
+  command     => 'usr/bin/pkill killmenow',
+  path        => 'usr/bin:/bin',
+  refreshonly => true,
+  logoutput   => true,
+}
