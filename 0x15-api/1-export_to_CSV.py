1783bd93e2adb5f62a56c6202e0d82c5251543db
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-22 16:20:55 -0700

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,7 +2,7 @@
 """
 Gather data from an API
 """
-import json
+import csv
 import requests
 import sys
 
@@ -10,3 +10,20 @@ import sys
 if __name__ == "__main__":
     ID = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com'
+    employeeURL = "{}/users/{}".format(url, ID)
+
+    res = requests.get(employeeURL)
+
+    todo_url = "{}/users/{}/todos".format(url, ID)
+
+    res2 = requests.get(todo_url)
+
+    filename = "{}.csv".format(ID)
+    username = res.json().get("username")
+
+    with open(filename, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for task in res2.json():
+            comp = task.get("completed")
+            title = str(task.get("title"))
+            writer.writerow([ID, username, comp, title])
