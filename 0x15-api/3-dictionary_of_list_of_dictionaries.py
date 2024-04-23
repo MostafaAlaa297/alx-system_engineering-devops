515cdbbdf79c277008118893b60febc4670b6ca3
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-22 17:00:25 -0700

    dict list of dicts

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,5 +8,33 @@ import sys
 
 
 if __name__ == "__main__":
-    ID = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com'
+    employeeURL = "{}/users".format(url)
+
+    filename = "todo_all_employees.json"
+
+    res = requests.get(employeeURL)
+
+    users = {}
+    for user in res.json():
+        users.update({user.get("id"): user.get("username")})
+
+    data = {}
+    for key, value in users.items():
+        todo_url = "{}/users/{}/todos".format(url, key)
+        res2 = requests.get(todo_url)
+
+        all_tasks = []
+
+        for task in res2.json():
+            tasks = {
+                    "username": value,
+                    "task": task.get("title"),
+                    "completed": task.get("completed")
+                    }
+            all_tasks.append(tasks)
+
+        data.update({key: all_tasks})
+
+    with open(filename, "w") as json_file:
+        json.dump(data, json_file)
