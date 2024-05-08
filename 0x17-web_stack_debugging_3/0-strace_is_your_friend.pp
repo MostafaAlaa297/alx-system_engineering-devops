68badc717cee4cde6e404dbbc42f53d0b6f0f8e7
MostafaAlaa297 <elaskary609@gmail.com>
2024-05-08 15:28:03 -0700

    debug stack

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,18 @@
+# Puppet manifest to fix Apache 500 error
+
+# Ensure Apache service is running
+service { 'apache2':
+    ensure => 'running',
+}
+
+# Fix Apache configuration file
+file { '/etc/apache2/apache2.conf':
+    ensure => present,
+    content => template('apache/apache2.conf.erb'),
+    notify => Service['apache2'],
+}
+
+# Ensure required packages are installed
+package { 'apache2':
+    ensure => 'installed',
+}
