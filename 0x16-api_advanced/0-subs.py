9e8130a75c65567ce4301fced142f3aa47d7d944
MostafaAlaa297 <elaskary609@gmail.com>
2024-05-08 16:08:32 -0700

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Connect to Reddit API
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Get the number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
