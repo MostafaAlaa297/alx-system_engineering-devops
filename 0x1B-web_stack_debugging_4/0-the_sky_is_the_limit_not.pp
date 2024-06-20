a3521f7d6738cd5dad9f07f13f4865ae3572ef50
MostafaAlaa297 <elaskary609@gmail.com>
2024-06-20 14:20:40 -0700

    fix code

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,25 +1,3 @@
-# File: 0-the_sky_is_the_limit_not.pp
-
-# Ensure Nginx package is installed
-package { 'nginx':
-  ensure => 'installed',
-}
-
-service {'nginx':
-	ensure    => 'running',
-	enable    => true,
-	subscribe => File['/etc/nginx/nginx.conf'],
-}
-
-file {'/etc/nginx/nginx.conf':
-	ensure  => 'file',
-	content => template('nginx/nginx.conf.erb'),
-	notify  => Service['nginx'],
-}
-
-exec {'optimize_nginx':
-	command     => '/usr/sbin/nginx -s reload',
-	path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-	refreshonly => true,
-	subscribe   => File['/etc/nginx/nginx.conf'],
-}
+# DOCS
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
